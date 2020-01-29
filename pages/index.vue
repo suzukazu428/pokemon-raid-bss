796ec9b0bd47d7828c52010dd44bf571a948c2e7
鈴木　和弥 <>
2020-01-29 09:50:55 +0900

    基本機能を実装した

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,48 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        pokemon-raid-bss
-      </h1>
-      <h2 class="subtitle">
-        POKEMONsword&amp;shield-Raid-BulletinBoard
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section>
+    <textarea
+      v-model="message"
+      placeholder="Please enter a comment(Within 100 characters)"
+      maxLength="100"
+    />
+    <button @click="sendData">
+      Submit
+    </button>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.message }}
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import { mapGetters } from 'vuex';
+import firebase from '@/plugins/firebase';
+const db = firebase.firestore();
 
 export default {
-  components: {
-    Logo
-  }
-}
+  data() {
+    return {
+      message: '',
+    };
+  },
+  computed: {
+    ...mapGetters(['posts']),
+  },
+  created() {
+    this.$store.dispatch('setPostsRef', db.collection('posts'));
+  },
+  methods: {
+    sendData() {
+      if (!this.message || this.message.length > 100) {
+        return false;
+      }
+      const dbdata = {
+        message: this.message,
+      };
+      db.collection('posts').add(dbdata);
+    },
+  },
+};
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
