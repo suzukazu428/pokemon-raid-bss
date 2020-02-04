405f489e4de16a54e93889c6d26410b82f11ea96
鈴木　和弥 <>
2020-02-04 10:28:42 +0900

    basebutton作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <section>
-    <textarea
-      v-model="message"
-      placeholder="Please enter a comment(Within 100 characters)"
-      maxLength="100"
-    />
-    <button @click="sendData">
-      Submit
-    </button>
+  <section class="flex justify-between">
+    <div>
+      <textarea
+        v-model="message"
+        placeholder="Please enter a comment(Within 100 characters)"
+        maxLength="100"
+      />
+      <BaseButton @click="sendData" class="w-24 h-12" bg-color="#E7A4C4">
+        Submit
+      </BaseButton>
+    </div>
     <ul>
       <li v-for="post in posts" :key="post.id">
         {{ post.message }}
@@ -19,9 +21,13 @@
 <script>
 import { mapGetters } from 'vuex';
 import firebase from '@/plugins/firebase';
+import BaseButton from '@/components/BaseButton';
 const db = firebase.firestore();
 
 export default {
+  components: {
+    BaseButton,
+  },
   data() {
     return {
       message: '',
